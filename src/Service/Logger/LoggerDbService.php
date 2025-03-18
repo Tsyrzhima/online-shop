928a4a06aa684f3ecbdbcbd8282d8405562aafd3
Tsyrzhima <sss1992bur@mail.ru>
2025-03-18 21:25:01 +0800

    Исправляет ошибку

--- src/Service/Logger/LoggerDbService.php
+++ src/Service/Logger/LoggerDbService.php
@@ -6,12 +6,12 @@ use Model\Logs;
 
 class LoggerDbService implements LoggerInterface
 {
-    private Logs $errorModel;
+    private Logs $logsModel;
     public function __construct(){
-        $this->errorModel = new Logs();
+        $this->logsModel = new Logs();
     }
     public function error(\Throwable $exception)
     {
-        $this->errorModel->add($exception->getMessage(),$exception->getFile(),$exception->getLine(),date('Y-m-d H:i:s'));
+        $this->logsModel->add($exception->getMessage(),$exception->getFile(),$exception->getLine(),date('Y-m-d H:i:s'));
     }
 }
